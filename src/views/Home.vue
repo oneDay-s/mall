3a770023cfc7f751742a06750c9ac9a78ea54197
songzq <1311268739@qq.com>
2020-09-20 16:44:37 +0800

    cros跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import  axios  from  'axios'
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    let url = 'http://www.easy-mock.com/mock/5f670f277304034f4b754175/mall/a/haha#!method=get';
+   axios.get(url).then(data => {
+     console.log(data)
+   })
   }
 }
 </script>
