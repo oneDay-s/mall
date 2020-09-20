01780c440512523e3454d72c7e243a9050c39ae7
songzq <1311268739@qq.com>
2020-09-20 21:49:26 +0800

    项目结构确认

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="home">
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import  axios  from  'axios'
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  mounted() {
+    let url = 'https://www.easy-mock.com/mock/5f670f277304034f4b754175/mall/a/haha#!method=get';
+   axios.get(url).then(data => {
+     console.log(data)
+   })
+  }
+}
+</script>
